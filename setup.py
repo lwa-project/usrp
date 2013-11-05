e7bcd858c009caf3b0044b45fa93bb983656526b
Jayce Dowell <jayce.dowell@gmail.com>
2013-11-05 18:45:51 +0000

    Switched teh various lsl_toolkits over to a setuptools-based build system.

--- setup.py
+++ setup.py
@@ -1,44 +1,33 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import ez_setup
+ez_setup.use_setuptools()
+
+import glob
+
+from setuptools import setup, Extension, Distribution, find_packages
+
 try:
 	import numpy
 except ImportError:
 	pass
-from distutils.core  import setup, Command
 
-packages          = ['lsl_toolkits','lsl_toolkits.USRP']
-package_dirs       = {'':''}
-
-class TestSuite(Command):
-	user_options = []
-	def initialize_options(self):
-		pass
-	def finalize_options(self):
-		pass
-	def run(self):
-		from tests import test_usrp
-		t = unittest.TestSuite()
-		r = unittest.TextTestRunner(verbosity=2)
-		loader = unittest.TestLoader()
-		t.addTests(loader.loadTestsFromModule(test_usrp))
-		r.run(t)
-
-cmdClasses = {'test': TestSuite}
 setup(
-  cmdclass          = cmdClasses,
-  name              = "USRP",
-  version           = "0.1.0",
-  description       = "Python reader for GNURadio/USRP data",
-  url               = "http://fornax.phys.unm.edu/lwa/trac/", 
-  author            = "Jayce Dowell",
-  author_email      = "jdowell@unm.edu",
-  license           = 'GPL',
-  classifiers       = ['Development Status :: 4 - Beta',
+  name                 = "USRP",
+  version              = "0.2.0",
+  description          = "Python reader for GNURadio/USRP data",
+  url                  = "http://fornax.phys.unm.edu/lwa/trac/", 
+  author               = "Jayce Dowell",
+  author_email         = "jdowell@unm.edu",
+  license              = 'GPL',
+  classifiers          = ['Development Status :: 4 - Beta',
 				   'Intended Audience :: Science/Research',
 				   'License :: OSI Approved :: GNU General Public License (GPL)',
 				   'Topic :: Scientific/Engineering :: Astronomy'],
-  packages          = packages,
-  package_dir       = package_dirs,
-  scripts           = ['scripts/usrpCheckTimetags.py', 'scripts/usrpFileCheck.py', 'scripts/usrpSpectra.py', 'scripts/usrpTimeseries.py', 'scripts/usrpWaterfall.py']
+  packages             = find_packages(exclude="tests"), 
+  namespace_packages   = ['lsl_toolkits',],
+  scripts              = glob.glob('scripts/*.py'), 
+  include_package_data = True,  
+  zip_safe             = False,  
+  test_suite           = "tests.test_usrp.usrp_tests"
 )
