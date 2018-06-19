73a549393e52c078e36baa7988def6cea8a03967
Jayce Dowell <jayce.dowell@gmail.com>
2018-06-19 15:26:42 +0000

    Worked on making the lsl_toolkits.USRP module PEP8 compliant.  Also, worked on modernizing the codebase a little in order to get ready for Python3.

--- scripts/usrpCombinePols.py
+++ scripts/usrpCombinePols.py
@@ -16,53 +16,53 @@ import numpy
 
 
 def main(args):
-	# Sort and read in the data
-	filename1 = args[0]
-	filename2 = args[1]
-	
-	dd1 = numpy.load(filename1)
-	dd2 = numpy.load(filename2)
-	
-	# Verify that the files are from the data data collection/processing batch
-	for attr in ['tInt', 'srate', 'freq1', 'times']:
-		try:
-			try:
-				## For simple things, a direct comparison
-				assert(dd1[attr] == dd2[attr])
-			except ValueError:
-				## For arrays, check the shape and all values
-				assert(dd1[attr].shape == dd2[attr].shape)
-				assert((numpy.abs(dd1[attr]-dd2[attr])**2).sum() == 0)
-				
-		except AssertionError:
-			raise RuntimeError("Attribute '%s' does not match between the two files." % attr)
-			
-	# Output
-	srate = dd1['srate']
-	tInt = dd1['tInt']
-	freq = dd1['freq']
-	freq1 = dd1['freq1']
-	freq2 = dd1['freq2']
-	times = dd1['times']
-	standMapper = dd1['standMapper']
-	
-	spec = numpy.zeros_like(dd1['spec'])
-	if filename1.find('_pol0'):
-		spec[:,0,:] = dd1['spec'][:,0,:]
-		spec[:,1,:] = dd2['spec'][:,0,:]
-	else:
-		spec[:,0,:] = dd2['spec'][:,0,:]
-		spec[:,1,:] = dd1['spec'][:,0,:]
-		
-	filename = filename1.replace('_pol0', '_comb').replace('_pol1', '_comb')
-	filename = os.path.basename(filename)
-	numpy.savez(filename, srate=srate, tInt=tInt, freq=freq, freq1=freq1, freq2=freq2, times=times, spec=spec, standMapper=standMapper)
-	
-	# Close
-	dd1.close()
-	dd2.close()
+    # Sort and read in the data
+    filename1 = args[0]
+    filename2 = args[1]
+    
+    dd1 = numpy.load(filename1)
+    dd2 = numpy.load(filename2)
+    
+    # Verify that the files are from the data data collection/processing batch
+    for attr in ['tInt', 'srate', 'freq1', 'times']:
+        try:
+            try:
+                ## For simple things, a direct comparison
+                assert(dd1[attr] == dd2[attr])
+            except ValueError:
+                ## For arrays, check the shape and all values
+                assert(dd1[attr].shape == dd2[attr].shape)
+                assert((numpy.abs(dd1[attr]-dd2[attr])**2).sum() == 0)
+                
+        except AssertionError:
+            raise RuntimeError("Attribute '%s' does not match between the two files." % attr)
+            
+    # Output
+    srate = dd1['srate']
+    tInt = dd1['tInt']
+    freq = dd1['freq']
+    freq1 = dd1['freq1']
+    freq2 = dd1['freq2']
+    times = dd1['times']
+    standMapper = dd1['standMapper']
+    
+    spec = numpy.zeros_like(dd1['spec'])
+    if filename1.find('_pol0'):
+        spec[:,0,:] = dd1['spec'][:,0,:]
+        spec[:,1,:] = dd2['spec'][:,0,:]
+    else:
+        spec[:,0,:] = dd2['spec'][:,0,:]
+        spec[:,1,:] = dd1['spec'][:,0,:]
+        
+    filename = filename1.replace('_pol0', '_comb').replace('_pol1', '_comb')
+    filename = os.path.basename(filename)
+    numpy.savez(filename, srate=srate, tInt=tInt, freq=freq, freq1=freq1, freq2=freq2, times=times, spec=spec, standMapper=standMapper)
+    
+    # Close
+    dd1.close()
+    dd2.close()
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
 
--- setup.py
+++ setup.py
@@ -8,26 +8,32 @@ import glob
 from setuptools import setup, Extension, Distribution, find_packages
 
 try:
-	import numpy
+    import numpy
 except ImportError:
-	pass
+    pass
 
 setup(
-  name                 = "USRP",
-  version              = "0.2.2",
-  description          = "Python reader for GNURadio/USRP data",
-  url                  = "http://fornax.phys.unm.edu/lwa/trac/", 
-  author               = "Jayce Dowell",
-  author_email         = "jdowell@unm.edu",
-  license              = 'GPL',
-  classifiers          = ['Development Status :: 4 - Beta',
-				   'Intended Audience :: Science/Research',
-				   'License :: OSI Approved :: GNU General Public License (GPL)',
-				   'Topic :: Scientific/Engineering :: Astronomy'],
-  packages             = find_packages(exclude="tests"), 
-  namespace_packages   = ['lsl_toolkits',],
-  scripts              = glob.glob('scripts/*.py'), 
-  include_package_data = True,  
-  zip_safe             = False,  
-  test_suite           = "tests.test_usrp.usrp_tests"
+    name                 = "USRP",
+    version              = "0.2.3",
+    description          = "Python reader for GNURadio/USRP data",
+    url                  = "https://fornax.phys.unm.edu/lwa/trac/", 
+    author               = "Jayce Dowell",
+    author_email         = "jdowell@unm.edu",
+    license              = 'GPL',
+    classifiers          = ['Development Status :: 5 - Production/Stable',
+                            'Intended Audience :: Developers',
+                            'Intended Audience :: Science/Research',
+                            'License :: OSI Approved :: GNU General Public License (GPL)',
+                            'Topic :: Scientific/Engineering :: Astronomy',
+                            'Programming Language :: Python :: 2',
+                            'Programming Language :: Python :: 2.6',
+                            'Programming Language :: Python :: 2.7',
+                            'Operating System :: MacOS :: MacOS X',
+                            'Operating System :: POSIX :: Linux'],
+    packages             = find_packages(exclude="tests"), 
+    namespace_packages   = ['lsl_toolkits',],
+    scripts              = glob.glob('scripts/*.py'), 
+    include_package_data = True,  
+    zip_safe             = False,  
+    test_suite           = "tests.test_usrp.usrp_tests"
 )
