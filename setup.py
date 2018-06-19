0377b5aba9dd2eb69742810f5ff198ddd23d37ef
Jayce Dowell <jayce.dowell@gmail.com>
2018-06-19 16:54:57 +0000

    Updated the names of the various LSL toolkits to get them ready for PyPi.

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import ez_setup
-ez_setup.use_setuptools()
-
 import glob
 
 from setuptools import setup, Extension, Distribution, find_packages
@@ -13,7 +10,7 @@ except ImportError:
     pass
 
 setup(
-    name                 = "USRP",
+    name                 = "lsl-toolkits-usrp",
     version              = "0.2.3",
     description          = "Python reader for GNURadio/USRP data",
     url                  = "https://fornax.phys.unm.edu/lwa/trac/", 
@@ -33,6 +30,9 @@ setup(
     packages             = find_packages(exclude="tests"), 
     namespace_packages   = ['lsl_toolkits',],
     scripts              = glob.glob('scripts/*.py'), 
+    python_requires      = '>=2.6, <3', 
+    setup_requires       = ['numpy>=1.2'], 
+    install_requires     = ['numpy>=1.2'],
     include_package_data = True,  
     zip_safe             = False,  
     test_suite           = "tests.test_usrp.usrp_tests"
